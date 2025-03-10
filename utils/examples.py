cf1d7ac12e3b0d1a55c06e892e36af237dcef57e
Izabelle Tais <ohmadorito@gmail.com>
2025-03-10 14:39:50 +0000

    feat: added create_user_table function

---
+++ utils/examples.py
@@ -0,0 +1,11 @@
+#MOVIE TABLE
+movie_titles = ['Inception', 'The Dark Knight', 'The Shawshank Redemption', 'Pulp Fiction',
+'The Godfather', 'Forrest Gump', 'Gladiator', 'The Matrix', 'Interstellar', 'The Lion King']
+
+movie_directors = []
+
+movie_genres = []
+
+movie_release_dates = []
+
+movie_durations = []
